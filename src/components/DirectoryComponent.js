47f1c6921264b0b145d42a6541718cadf0658271
Ross Gordon <rossagordonstl@gmail.com>
2022-02-06 20:12:29 -0500

    Combining Reducers

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,7 +1,6 @@
-
 import React from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import CampsiteInfo from './CampsiteInfoComponent';
 
 function RenderDirectoryItem({campsite}){
